44524e78298561083dafd4e2c58a8e5ff1a4b984
pgagnidze <pgagnidze@pm.me>
2023-06-04 00:06:35 +0400

    feat: vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,18 +3,22 @@ import { Inter } from "@next/font/google";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { ErrorBoundary } from "react-error-boundary";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function AppContent({ Component, pageProps }) {
   return (
-    <div className="relative">
-      <Header />
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className="relative">
+        <Header />
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+      <Analytics />
+    </>
   );
 }
 
@@ -26,10 +30,12 @@ function handleError(error) {
       errorMessage = parsed.error.message;
     } catch (e) {}
     if (errorMessage.includes("exceeded your current quota")) {
-      errorMessage = "You exceeded your current quota, please check your plan and billing details.";
+      errorMessage =
+        "You exceeded your current quota, please check your plan and billing details.";
     }
     if (errorMessage.includes("maximum context length")) {
-      errorMessage = "You have reached to your maximum token limit, please start a new conversation.";
+      errorMessage =
+        "You have reached to your maximum token limit, please start a new conversation.";
     }
     alert(`From OpenAI: ${errorMessage}`);
   }
