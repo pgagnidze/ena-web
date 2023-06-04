6d1041c71b2d4d0c32bd93bf26d4e547c494dd01
pgagnidze <pgagnidze@pm.me>
2023-06-04 17:04:48 +0400

    fix: test gtranslate domain

--- utils/cors.ts
+++ utils/cors.ts
@@ -4,7 +4,7 @@ const whitelist = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
   "https://ena-lang.org",
-  "https://ena--lang-org.translate.goog",
+  // "https://ena--lang-org.translate.goog",
 ];
 
 export const allowCors =
